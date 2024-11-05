acb12cc63778324c683aee046bcc5da1faec3a3c
sho <122590330+sho-mb@users.noreply.github.com>
2024-11-05 10:49:06 +0700

    Add files via upload

---
+++ Character Picture Grid.py
@@ -0,0 +1,19 @@
+#Character Picture Grid
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+
+def gridPrinter(grid):
+    for x in range(len(grid[0])):
+        for y in range(len(grid)):
+            print(grid[y][x], end="")
+        print()
+gridPrinter(grid)
